9d2fe7bb5df273e811f65ffcd40cf2d27af0f39c
Kenny Hung <kennysubs+github@outlook.com>
2018-08-04 22:09:06 +0100

    Init project

---
+++ src/main/kotlin/swynck/app/Run.kt
@@ -0,0 +1,3 @@
+package swynck.app
+
+fun main(args: Array<String>) = println("Hello world")
---
+++ src/test/kotlin/swynck/app/RunTests.kt
@@ -0,0 +1,11 @@
+package swynck.app
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class RunTests {
+    @Test
+    fun `dummy test`() {
+        assertThat("hello").isEqualTo("hello")
+    }
+}
