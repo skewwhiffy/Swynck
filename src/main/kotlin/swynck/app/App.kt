ecc21e4ae102e393994dba9334e49f1fbdd25ccd
Kenny Hung <kennysubs+github@outlook.com>
2018-08-04 22:46:35 +0100

    API /ping endpoint

---
+++ src/main/kotlin/swynck/app/App.kt
@@ -0,0 +1,13 @@
+package swynck.app
+
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+
+object App {
+    operator fun invoke() = routes(
+        "/ping" bind GET to { Response(OK).body("pong") }
+    )
+}
--- src/main/kotlin/swynck/app/Run.kt
+++ src/main/kotlin/swynck/app/Run.kt
@@ -1,3 +1,12 @@
 package swynck.app
 
+import org.http4k.server.Undertow
+import org.http4k.server.asServer
+
 fun main(args: Array<String>) = println("Hello world")
+
+object Run {
+    operator fun invoke(port: Int) {
+        App().asServer(Undertow(port)).start()
+    }
+}
