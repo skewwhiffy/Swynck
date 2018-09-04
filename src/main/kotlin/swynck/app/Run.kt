1f7b6229dd0bff7e486cb23f8e96202e515cabbf
Kenny Hung <kennysubs+github@outlook.com>
2018-09-04 20:23:13 +0100

    T3 Daemon runner, first pass WIP

--- src/main/kotlin/swynck/app/Run.kt
+++ src/main/kotlin/swynck/app/Run.kt
@@ -2,6 +2,8 @@ package swynck.app
 
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
+import swynck.daemon.DaemonRunner
+import swynck.daemon.task.FileDetailsSync
 import swynck.db.Migrations
 
 fun main(args: Array<String>) = Run(Dependencies())
@@ -15,6 +17,9 @@ object Run {
             dependencies.userRepository,
             dependencies.oneDrive
         ).asServer(Undertow(dependencies.config.port())).start()
-        println("Server started")
+        println("Starting daemon runner")
+        val daemon = DaemonRunner()
+        dependencies.userRepository.getUser()?.let { FileDetailsSync(it) }?.let { daemon.add(it) }
+        println("Swynck started")
     }
 }
--- src/main/kotlin/swynck/daemon/DaemonRunner.kt
+++ src/main/kotlin/swynck/daemon/DaemonRunner.kt
@@ -1,12 +1,44 @@
 package swynck.daemon
 
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import org.h2.mvstore.ConcurrentArrayList
 import swynck.daemon.task.DaemonTask
+import java.time.Duration
+import java.time.Instant
+import kotlin.streams.asStream
 
 class DaemonRunner {
+    private val runnerTask: Deferred<Nothing>
+    init {
+        runnerTask = async {
+            start()
+        }
+    }
+
     private val tasks = ConcurrentArrayList<DaemonTask>()
 
     fun add(task: DaemonTask) {
         tasks.add(task)
+    }
+
+    private suspend fun start(): Nothing {
+        var lastRun = Instant.now()
+        while (true) {
+            val nextRun = lastRun.plusSeconds(1)
+            val now = Instant.now()
+            if (nextRun > now) {
+                val wait = Duration.between(now, nextRun)
+                delay(wait.toMillis())
+            }
+            val runs = tasks.iterator().asSequence().map { async { it.run() } }
+            // TODO: There must be a nicer way to do this
+            while (runs.any { it.isActive }) {
+                delay(50)
+            }
+            lastRun = Instant.now()
+        }
     }
 }
