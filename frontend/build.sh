38fb60453cb86ecfbc2a10d30502aef25be53eca
Kenny Hung <kennysubs+github@outlook.com>
2018-11-18 16:27:07 +0000

    Switch to yarn (#6)

--- frontend/build.sh
+++ frontend/build.sh
@@ -10,14 +10,18 @@ case "$(uname -s)" in
         sed -i -e 's/http:\/\/localhost:38080//' src/config/Config.js
         ;;
 esac
+
 echo Installing dependencies...
-npm install
+./yarn.sh install
+
 echo Compiling JavaScript, an interpreted language...
-npm run-script build
+./yarn.sh build
+
 echo Embedding static www into Kotlin solution...
 mkdir -p ../src/main/resources/www
 rm -fr ../src/main/resources/www
 mv build ../src/main/resources/www
+
 echo Creating test.js for testing...
 touch ../src/main/resources/www/static/test.js
 echo $(date) > ../src/main/resources/www/static/test.js
