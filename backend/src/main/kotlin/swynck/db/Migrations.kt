847b24feadc76cdc999af26211499aabb0431432
Kenny Hung <kenny.hung@oliverwyman.com>
2018-12-29 16:58:54 +0000

    Upgrade all dependencies to the latest and greatest

--- backend/src/main/kotlin/swynck/db/Migrations.kt
+++ backend/src/main/kotlin/swynck/db/Migrations.kt
@@ -4,9 +4,9 @@ import org.flywaydb.core.Flyway
 import swynck.app.Dependencies
 
 class Migrations(private val dependencies: Dependencies) {
-    fun run() {
-        val flyway = Flyway()
-        flyway.dataSource = dependencies.dataSourceFactory.dataSource()
-        flyway.migrate()
-    }
+    fun run() = Flyway
+        .configure()
+        .dataSource(dependencies.dataSourceFactory.dataSource())
+        .load()
+        .migrate()
 }
