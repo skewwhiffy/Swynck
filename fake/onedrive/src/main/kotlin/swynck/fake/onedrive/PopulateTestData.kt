3b6c625df9d3244a77e7a3ec7591d4dfae9039df
Kenny Hung <kenny.hung@oliverwyman.com>
2018-12-29 18:07:15 +0000

    Test data population gradle task

---
+++ fake/onedrive/src/main/kotlin/swynck/fake/onedrive/PopulateTestData.kt
@@ -0,0 +1,9 @@
+package swynck.fake.onedrive
+
+fun main(args: Array<String>) = PopulateTestData()
+
+object PopulateTestData {
+    operator fun invoke() {
+        println("Hello world")
+    }
+}
